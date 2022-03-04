675b33166f3fa1851a7a8f1a4e9472f4303f789f
Yasser Ahmed <iyasser007@gmail.com>
2022-03-04 22:11:19 +0300

    initial design and setup for login page

--- ChatApp/ContentView.swift
+++ ChatApp/ContentView.swift
@@ -8,9 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isLoginState: Bool = false
+    @State var email = ""
+    @State var password = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollView {
+                VStack {
+                    Text("my login page")
+                    Picker(selection: $isLoginState, label: Text("picker here"), content: {
+                        // The selection is determined by 'isLoginState'
+                        // false = Create Account, true = Login
+                        Text("Login")
+                            .tag(true)
+                        Text("Create Account")
+                            .tag(false)
+                    }).pickerStyle(SegmentedPickerStyle())
+                    Button(action: {
+                        
+                    }, label: {
+                        Image(systemName: "person.fill")
+                            .font(.system(size: 64))
+                            .accentColor(.black)
+                            .padding()
+                    })
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                        .padding()
+                    SecureField("Password", text: $password)
+                        .padding()
+                    Button(action: {
+                        
+                    }, label: {
+                        HStack {
+                            Spacer()
+                            Text("Create Account")
+                                .foregroundColor(Color.white)
+                                .padding(.vertical, 10)
+                            Spacer()
+                        }.background(Color.blue)
+                        .padding()
+                    })
+                }.padding()
+            }
+            .navigationTitle("Create Account")
+        }
     }
 }
 
